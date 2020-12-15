64656e5542f3015df2cf69bc90482fbe1779f4ca
Jonah Stewart <jonahlstewart@gmail.com>
2020-12-15 10:22:52 -0500

    resolved git conflicts

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,10 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 import IconSidebar from './components/Sidebar/IconSidebar';
 import Home from './pages/Home';
@@ -9,51 +14,34 @@ import Logs from './pages/Logs';
 import Streams from './pages/Streams';
 import Team from './pages/Team';
 import Support from './pages/Support';
-<<<<<<< HEAD
 import './App.scss';
-=======
 
->>>>>>> 67ae9dbd8893590ad4fa6dfdd977b4e6e5236439
-export default class App extends Component{
-  constructor(props, context){
+export default class App extends Component {
+  constructor(props, context) {
     super(props, context);
-    this.state={
-    }
+    this.state = {};
   }
-  componentDidMount(){
-  }
-  render(){
-    return(
+  componentDidMount() {}
+  render() {
+    return (
       <div className="appContainer">
         <Router>
           <Sidebar className="sidebar" />
           {/* <IconSidebar /> */}
-<<<<<<< HEAD
           {/* <Dashboard /> */}
           <div className="mainContainer">
             <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/dashboard' component={Dashboard} />
-                <Route path='/mirror' component={Mirror} />
-                <Route path='/logs' component={Logs} />
-                <Route path='/streams' component={Streams} />
-                <Route path='/team' component={Team} />
-                <Route path='/support' component={Support} />
+              <Route path="/" exact component={Home} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/mirror" component={Mirror} />
+              <Route path="/logs" component={Logs} />
+              <Route path="/streams" component={Streams} />
+              <Route path="/team" component={Team} />
+              <Route path="/support" component={Support} />
             </Switch>
           </div>
-=======
-        <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/dashboard' component={Dashboard} />
-            <Route path='/mirror' component={Mirror} />
-            <Route path='/logs' component={Logs} />
-            <Route path='/streams' component={Streams} />
-            <Route path='/team' component={Team} />
-            <Route path='/support' component={Support} />
-        </Switch>
->>>>>>> 67ae9dbd8893590ad4fa6dfdd977b4e6e5236439
         </Router>
       </div>
-    )
+    );
   }
 }
