ed94e917c8d3992d433cc1d81af1d7b2845e4df8
Joe Kinney <joe@enterpagoda.com>
2020-12-16 14:48:09 -0700

    reconfigure throughput chart and event chart

--- client/components/charts/ThroughputChart.jsx
+++ client/components/charts/ThroughputChart.jsx
@@ -1,62 +1,68 @@
-import React, { Component } from 'react';
-import { io } from 'socket.io-client';
-import interval from './chartConfig'
+import React, { Component } from "react";
+import { io } from "socket.io-client";
+import interval from "./chartConfig";
 // const io = require('socket.io-client');
 // const ioClient = io.connect('http://localhost:3030');
 // ioClient.on('log', (msg) => console.log(msg));
 
-
-class MyChart extends Component {
+class Throughput extends Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
-    let bytes =0
-    const socket= io('http://localhost:3030')
-    socket.on('log', (data)=>{
-      // bytes+=data
-      bytes+=JSON.parse(data).requestSize
-    })
+    let bytes = 0;
+    const socket = io("http://localhost:3030");
+    socket.on("log", (data) => {
+      bytes += JSON.parse(data).requestSize;
+    });
 
-    const ctx = document.getElementById('myChart').getContext('2d');
+    const ctx = document.getElementById("myChart").getContext("2d");
     const liveChart = new Chart(ctx, {
       // The type of chart we want to create
-      type: 'bar',
+      type: "bar",
 
       // The data for our dataset
       data: {
-        labels: Array(100).fill(0).map(x => 0),
-        datasets: [{
-          lineTension: 0,
-          label: 'live chart',
-          backgroundColor: 'rgba(115, 115, 217, 1)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: Array(100).fill(0).map(x => 0),
-        }]
+        labels: Array(100)
+          .fill(0)
+          .map((x) => 0),
+        datasets: [
+          {
+            lineTension: 0,
+            label: "live chart",
+            backgroundColor: "rgba(115, 115, 217, 1)",
+            borderColor: "rgb(255, 99, 132)",
+            data: Array(100)
+              .fill(0)
+              .map((x) => 0),
+          },
+        ],
       },
       // Configuration options go here
       options: {
         animation: {
           tension: {
             duration: 100,
-            easing: 'linear',
+            easing: "linear",
             from: 1,
             to: 0,
-            loop: true
-          }
+            loop: true,
+          },
         },
         legend: {
-          display: false
+          display: false,
         },
         scales: {
-          yAxes: [{
-            display: true,
-            ticks: {
-              min: 0
-            }
-          }],
-        }
-      }
+          yAxes: [
+            {
+              display: true,
+              ticks: {
+                min: 0,
+              },
+            },
+          ],
+        },
+      },
     });
 
     function addData(chart, label, data) {
@@ -65,7 +71,7 @@ class MyChart extends Component {
         dataset.data.push(data);
       });
       chart.update();
-      return
+      return;
     }
     //============================================
 
@@ -78,16 +84,16 @@ class MyChart extends Component {
         dataset.data.shift();
       });
       chart.update();
-      return
+      return;
     }
 
     function chartAnimate(chart, label, data) {
-      let copy=data
+      let copy = data;
       addData(chart, label, data);
       //======================
       removeData(chart);
       //----------------------
-      return
+      return;
     }
 
     function randomizeCallout() {
@@ -96,40 +102,31 @@ class MyChart extends Component {
       // },interval)
       // console.log('\n\nbytes: ',bytes)
       // console.log('bytes after cleaned: ',bytes)
-      let time = ''
+      let time = "";
       let d = new Date();
-      time += d.getHours() + ' : ';
-      time += d.getMinutes() + ' : ';
-      time += d.getSeconds();// + ' : ';
+      time += d.getHours() + " : ";
+      time += d.getMinutes() + " : ";
+      time += d.getSeconds(); // + ' : ';
       //time += d.getMilliseconds()
-      let val=bytes
-      console.log('bytes: ',val)
-      bytes=0
-      return chartAnimate(liveChart, time, val)
+      let val = bytes;
+      console.log("bytes: ", val);
+      bytes = 0;
+      return chartAnimate(liveChart, time, val);
     }
     // randomizeCallout();
     setInterval(() => {
-      randomizeCallout()
+      randomizeCallout();
     }, interval);
   }
   render() {
     return (
-    <div className={this.props.className}>
-      <div className="moch-chart" >
-        <canvas id="myChart"></canvas>
+      <div className={this.props.className}>
+        <div className="moch-chart">
+          <canvas id="myChart"></canvas>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
-  ;
 }
 
-export default MyChart;
-
-
-
-
-
-
-
-
+export default Throughput;
