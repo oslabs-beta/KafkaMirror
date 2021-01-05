29219c2d7729cb725917c01495fbeef1c013eeb7
Bliss Crossroad <blisscrossroad@Tobi-Wans-MacBook-Pro.local>
2021-01-04 22:44:52 -0400

    settings port input

--- client/pages/Settings.js
+++ client/pages/Settings.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Pages.scss';
 
-function Settings() {
+const Settings = () => {
+  const [portNumber, setPortNumber] = useState('');
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setPortNumber(value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //post to server port
+    console.log(portNumber);
+    setPortNumber('');
+  }
+
   return (
     <div className='settings'>
-      <h1>Settings</h1>
+      <form className="form-container" onSubmit={ handleSubmit }>
+        <label className='form-label'>Connect to the Kafka broker you want to listen to: </label>
+        <input type='text' name='portNumber' value={ portNumber } onChange={ handleChange } className="form-input" placeholder="Enter port number..." />
+        <button type='submit' className='btn-connect'>CONNECT</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Settings;
